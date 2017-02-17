f5da7926e788222b77f4ad00c0a3742423f1f626
RajuRJ <rajutharik@gmail.com>
2017-02-17 17:07:08 +0530

    This is my first commit

---
+++ shoppingcart/src/main/java/com/niit/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.niit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView showHomePage()
+	{
+		System.out.println("Starting the method showHomePage");
+		ModelAndView mv = new ModelAndView ("/HomePage");
+		mv.addObject("msg","WELCOME TO SHOPPING CART");
+		return mv;
+	}
+	@RequestMapping("Login")
+	public ModelAndView showLoginPage()
+	{
+		System.out.println("Clicked on Login Link");
+		ModelAndView mv = new ModelAndView ("/Login");
+		mv.addObject("isUserClickedLogin","true");
+		
+		return mv;
+	}
+	@RequestMapping("Register")
+	public ModelAndView showRegisterPage()
+	{
+		System.out.println("Clicked on Register Link");
+		ModelAndView mv = new ModelAndView ("/Register");
+		mv.addObject("isUserClickedRegister","true");
+		return mv;
+   }
+	@RequestMapping("validate")
+	public ModelAndView validateCredentials(@RequestParam("userID") String id,
+			@RequestParam("password") String pwd)
+	{
+		//Actually you have get the data from DB
+		//Temporarily -user->niit password=niit@123
+		ModelAndView mv = new ModelAndView ("/HomePage");
+		
+		if(id.equals("niit") && pwd.equals("niit@123"))
+	{
+			mv.addObject("msg","valid credentials");
+		
+	}
+		else
+		{
+			mv.addObject("msg", "Invalid Credentials...please try again");
+		}
+		return mv;
+	}
+	
+}
---
+++ shoppingcart/src/main/webapp/WEB-INF/jsp-pages/HomePage.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib
+    prefix="c"
+    uri="http://java.sun.com/jsp/jstl/core" 
+%>
+    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Shopping Cart WebSite</title>
+</head>
+<body>
+<center><h2>Shopping Cart</h2></center>
+<center> ${msg} </center>
+<hr>
+
+<a href="Login">Login</a><br>
+<a href="Register">Register</a>
+<br>
+
+<c:if test="{isUserClickedLogin==true}">
+<jsp:include page="Login.jsp"></jsp:include>
+</c:if>
+<c:if test="{isUserClickedRegister==true}">
+<jsp:include page="Register.jsp"></jsp:include>
+</c:if>     
+
+</body>
+</html>
